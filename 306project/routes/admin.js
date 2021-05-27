9e19ca350a5fe38d5a98e65f0b201cf5f66cad48
root <root@debian>
2021-05-27 22:56:10 +0300

    project

---
+++ 306project/routes/admin.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', isAuth, adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post(
+  '/add-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
+
+// gelecek 2liyi ben ekledim
+// pathleri i sor ve değiştir
+//buna göre bid sectionı için yeni path yarat shop.js te
+
+router.get('/edit-product/:productId', isAuth, adminController.giveBidProduct);
+
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postBidProduct
+);
+
+//////////////////////////////////
+
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
+
+module.exports = router;
